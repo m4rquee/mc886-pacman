5cc875564c154c4f3cd8eb20a54af25235ae269c
tsolera <thiagossolera@gmail.com>
2021-07-14 22:06:24 -0300

    punish stalling and reward moving forward

--- RlearningAgents.py
+++ RlearningAgents.py
@@ -74,6 +74,15 @@ class RlearningAgent(Agent):
             last_state = self.lastStates[-1]
             last_action = self.lastActions[-1]
 
+            # general rewarding for positive delta score
+            if reward > 0:
+                reward += 500
+            # Punish stalling and reward moving forward
+            if len(self.lastActions) >= 2 and last_action == Directions.REVERSE[self.lastActions[-2]]:
+                reward -= 10000
+            if len(self.lastActions) >= 2 and last_action == self.lastActions[-2]:
+                reward += 1000
+
             max_q = self.getMaxQ(curr_state, legal)
             self.updateQ(last_state, last_action, reward, max_q)
 
